1f032c628a53226009edfee9f80f8a869ceb1186
Alexey Badenkov <alexey.badenkov@gmail.com>
2013-03-17 13:24:21 +0400

    Init new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photobank::Application.config.secret_key_base = '5562d0b4cdc7cd4d861ece358145e1dec5eb89dca7c06dafb3204e91dfa33d18f1d3a86676fa3cdb36e562862ebd41f01f74e6e721af18a485b5cc874831612c'
